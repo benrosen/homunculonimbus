2aed0ba62565a1132b2cf11c488f8f8dd9b6d650
Ben Rosen <benjamin.p.rosen@gmail.com>
2023-02-15 21:28:28 -0500

    generate ast from graphql queries and mutations

--- lib/api/api.graphql.ts
+++ lib/api/api.graphql.ts
@@ -1,12 +1,53 @@
-import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { APIGatewayProxyResult, AppSyncResolverEvent } from "aws-lambda";
+import gql from "graphql-tag";
+
+// derive dynamodb commands from AST
+// issue commands and collect responses
+// apply postprocessing
+// return response
 
 exports.handler = async (
-  event: APIGatewayEvent,
-  context: Context
+  event: AppSyncResolverEvent<any>
 ): Promise<APIGatewayProxyResult> => {
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
 
-  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+  const parentTypeName = event.info.parentTypeName;
+
+  console.log(`Parent Type Name: ${parentTypeName}`);
+
+  const fieldName = event.info.fieldName;
+
+  console.log(`Field Name: ${fieldName}`);
+
+  const propertyQuotationMarkMatcher = /"(?=.*:)/g;
+
+  console.log(
+    `Property Quotation Mark Matcher: ${propertyQuotationMarkMatcher}`
+  );
+
+  const requestArguments = JSON.stringify(event.arguments)
+    .slice(1, -1)
+    .replace(propertyQuotationMarkMatcher, "");
+
+  console.log(`Arguments: ${requestArguments}`);
+
+  const selection = event.info.selectionSetGraphQL;
+
+  console.log(`Selection: ${selection}`);
+
+  const reconstructedQueryString = `${parentTypeName.toLowerCase()} {
+  ${fieldName}${requestArguments ? `(${requestArguments})` : ""} ${selection}
+}`;
+
+  console.log(
+    `Reconstructed GraphQL Query String: ${reconstructedQueryString}`
+  );
+
+  const ast = gql`
+    ${reconstructedQueryString}
+  `;
+
+  console.log(`AST: ${JSON.stringify(ast, null, 2)}`);
 
   return {
     statusCode: 501,
